de15b06398506db97e29e4006dd9eda29de820d2
evaZQR <2539906978@qq.com>
2024-06-06 16:56:26 +0800

    update the Readme.md

--- README.md
+++ README.md
@@ -16,4 +16,36 @@ pip install -r requirements.txt
 
 To vertify if you are successful, run test.ipynb, which finish the fundamental functions of llama-index
 
-# config
+# Config
+Firstly, you need to set your .env file 
+```
+# The information of the QQ-bot, if you don not have please create it at https://q.qq.com/ first.
+APPID = 
+TOKEN = 
+APPSECRET = 
+
+
+# OPENAI key
+API_SECRET_KEY = 
+BASE_URL = 
+
+# language
+LANGUAGE = Chinese
+
+# Weather key
+TOMORROW_API = 
+LOCATION = 
+
+# Azure OpenAI Service
+AZURE_OPENAI_KEY=
+AZURE_OPENAI_ENDPOINT=
+```
+since the project is not finished yet, the .env file will update while the project is developing
+
+
+# Our target
+* Our short term goal is to learn how to use the llama-index library, and learn how to make the Vector Rag and the Graph Rag, comparing these functions and make a better one to over the efficiency of the previous.
+* Our long term goal is to make a chat robot with memory throughout these Rag, this can be the agent that collecting data for you, it can search online and talk to you with its own memory.
+
+# Join us
+If you are interested in this project, please contact us.
--- chat.py
+++ chat.py
@@ -18,39 +18,28 @@ LANGUAGE = os.getenv("LANGUAGE")
 import json
 
 def store_the_history(history, **kwargs):
-    # 创建一个包含所有信息的字典
     data_to_store = {
         'history': history,
         'kwargs': kwargs
     }
-    
-    # 打印所有关键字参数
     print("The history is storing, find the following args:")
     for key, value in kwargs.items():
         print(f"{key}: {value}")
     
-    # 指定要写入的文件名
     filename = "./data/memory/dialog_history.json"
     
-    # 读取现有文件内容，如果文件不存在，则创建一个新的空数组
     try:
         with open(filename, 'r', encoding='utf-8') as file:
-            # 读取JSON数据
             existing_data = json.load(file)
-            # 确保existing_data是一个列表
             if not isinstance(existing_data, list):
                 raise ValueError("Existing data is not a list")
     except (FileNotFoundError, ValueError):
-        # 文件不存在或数据格式不正确，创建一个新的列表
         existing_data = []
     
-    # 将新数据添加到列表中
     existing_data.append(data_to_store)
     
-    # 将整个列表转换为JSON格式字符串
     json_data = json.dumps(existing_data, ensure_ascii=False, indent=4)
     
-    # 写入文件
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(json_data)
     
@@ -91,6 +80,5 @@ def start_chat_wo_memory(observation, model = "gpt-3.5-turbo"):
     print(history)
     store_the_history(history, observation=observation, time=get_current_time())
     
-
 if __name__ == "__main__":
     start_chat_wo_memory("接收到来自好友江海共余生的QQ信息:签到了吗？")
